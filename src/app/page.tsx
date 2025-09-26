93542259daba7a570127da13ba241fad96ffb2ed
Rafee <rafeejenkins@gmail.com>
2025-09-26 10:49:19 +0100

    feat: move logic to holiday-list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,8 @@
 "use client";
-import { useQueryState } from "nuqs";
-import { useMemo } from "react";
 import { Filters } from "@/features/filters/filters";
 import { HolidayList } from "@/features/holiday-list/holiday-list";
-import { useHolidayData } from "@/features/holiday-list/use-holiday-data";
 
 export default function Home() {
-  const { data, isLoading, error } = useHolidayData();
-  const [activeSort] = useQueryState("sort", {
-    defaultValue: "price",
-  });
-  const sortedHolidays = useMemo(() => {
-    if (!data) return [];
-    const holidaysCopy = [...data];
-    switch (activeSort) {
-      case "star":
-        return holidaysCopy.sort(
-          (a, b) => b.resort.starRating - a.resort.starRating,
-        );
-      case "name":
-        return holidaysCopy.sort((a, b) =>
-          a.resort.name.localeCompare(b.resort.name),
-        );
-      default:
-        return holidaysCopy.sort(
-          (a, b) =>
-            a.bookingDetails.price.amount - b.bookingDetails.price.amount,
-        );
-    }
-  }, [data, activeSort]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (error) return <div>Error loading holidays</div>;
-
   return (
     <div className="bg-otb-gradient">
       <section className="p-10 grid grid-cols-12">
@@ -41,7 +10,7 @@ export default function Home() {
           <Filters />
         </div>
         <div className="col-start-5 col-end-12">
-          <HolidayList holidays={sortedHolidays} />
+          <HolidayList />
         </div>
       </section>
     </div>
--- src/features/holiday-list/holiday-list.tsx
+++ src/features/holiday-list/holiday-list.tsx
@@ -1,14 +1,40 @@
-import type { Holiday } from "@/features/holiday-list/fetch-holidays";
+import { useQueryState } from "nuqs";
+import { useMemo } from "react";
 import { HolidayListItem } from "@/features/holiday-list/holiday-list-item";
+import { useHolidayData } from "@/features/holiday-list/use-holiday-data";
 
-interface Props {
-  holidays: Holiday[];
-}
+export const HolidayList = () => {
+  const { data, isLoading, error } = useHolidayData();
+  const [activeSort] = useQueryState("sort", {
+    defaultValue: "price",
+  });
+  const sortedHolidays = useMemo(() => {
+    if (!data) return [];
+    const holidaysCopy = [...data];
+    switch (activeSort) {
+      case "star":
+        return holidaysCopy.sort(
+          (a, b) => b.resort.starRating - a.resort.starRating,
+        );
+      case "name":
+        return holidaysCopy.sort((a, b) =>
+          a.resort.name.localeCompare(b.resort.name),
+        );
+      default:
+        return holidaysCopy.sort(
+          (a, b) =>
+            a.bookingDetails.price.amount - b.bookingDetails.price.amount,
+        );
+    }
+  }, [data, activeSort]);
 
-export const HolidayList = ({ holidays }: Props) => {
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error loading holidays</div>;
+
   return (
     <div className="flex gap-4 flex-col">
-      {holidays.map((holiday) => (
+      {sortedHolidays.map((holiday) => (
         <HolidayListItem key={holiday.resort.id} holiday={holiday} />
       ))}
     </div>
